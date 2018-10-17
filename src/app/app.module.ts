ae330b52338036e3cd44b6a3a9750a8253ebbb38
Joseph Alba <jalba19@gmail.com>
2018-10-17 19:47:35 -0400

    Added materialize.css to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MyFirstcomponentComponent } from './my-firstcomponent/my-firstcomponent.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+
+const routes: Routes = [
+  { path: 'home', component: MyFirstcomponentComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFirstcomponentComponent
+    MyFirstcomponentComponent,
+    MyNavComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
